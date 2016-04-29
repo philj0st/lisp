dfae27223e37f9905a07e6b3b3c37dcf43efdbf2
Phil <philx_x@hotmail.com>
2016-04-29 12:59:57 -0400

    add recursive way

--- 2.lisp
+++ 2.lisp
@@ -2,3 +2,12 @@
   (first (rest (reverse list)))
 )
 (print (my-but-last '(a b c d)))
+
+; improved recursive version
+(defun but-last (lst)
+  (if (caddr lst)
+    (but-last (cdr lst))
+    (car lst)
+  )
+)
+(print (but-last '(a b c d e f)))
